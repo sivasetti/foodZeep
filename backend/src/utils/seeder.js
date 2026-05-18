2cd10f162736c0426b12fe36f61f1ca85229f851
Siva Setti <113090286+sivasetti@users.noreply.github.com>
2026-05-18 23:59:22 +0530

    added seeder.js to create mock data for testing and created index for food name search

---
+++ backend/src/utils/seeder.js
@@ -0,0 +1,27 @@
+const db = require('../config/db');
+
+
+// 1. create an array of realistic mock food items
+
+const mockFoodItems = [
+    {
+        seller_id : 1,
+        name : 'Margherita Pizza',
+        quantity : 50,
+        veg : 1,
+        image_url : 
+    }
+];
+
+const seedDataBase = async () =>{
+    try{
+        console.log(`Starting database seeding`);
+
+        for(let food of mockFoodItems){
+            const query = `INSERT INTO food_items (name, price, description, )`
+        }
+    }
+    catch(error){
+
+    }
+}
