176027d0080f2a167cccc0032259882d226bee67
Siva Setti <113090286+sivasetti@users.noreply.github.com>
2026-05-21 00:32:12 +0530

    implement: Error Translation Mapping: rewrites status codes into human readable messages

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -1,6 +1,16 @@
 const logger = require('../config/logger');
 
 const errorHandler = (err, req, res, next) =>{
+
+    if(err.code === 'ER_DUP_ENTRY'){
+        err.statusCode = 400;
+        err.message = 'The email address or matching unique credential is already registered'
+    }
+    else if(err.code === 'ER_NO_REFERENCED_ROW_2'){
+        err.statusCode = 400;
+        err.message = 'The requested food item or entity does not exist in our system.';
+    }
+    
     const statusCode = err.statusCode || 500;
 
     //Structured Context Capture
