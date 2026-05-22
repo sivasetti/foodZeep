27f9e7f62ae671a16eb958f131b12d9195e04864
Siva Setti <113090286+sivasetti@users.noreply.github.com>
2026-05-22 23:10:59 +0530

    soft messaging validation for order placing

--- backend/src/validators/orders.validator.js
+++ backend/src/validators/orders.validator.js
@@ -1,7 +1,12 @@
 const Joi = require('joi');
 
 const checkOutSchema = Joi.object({
-    total_amount : Joi.number().precision(2).positive().required(),
+    total_amount : Joi.number().precision(2).positive().required()
+    .messages({
+        'number.base' : 'Total amount must be a number',
+        'number.positive' : 'Total amount must be greater than zero',
+        'any.required' : 'Total amount is a required field'
+    }),
     items : Joi.array().items(
         Joi.object({
             food_id : Joi.number().integer().required(),
@@ -9,6 +14,10 @@ const checkOutSchema = Joi.object({
             price : Joi.number().precision(2).positive().required()
         })
     ).min(1).required()
+    .messages({
+        'array.min' : 'An order must contain at least one food item',
+        'any.required' : 'Order items list is required'
+    })
 });
 
 
