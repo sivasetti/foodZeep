54994c6a7a02894cd50e02fc009df645a31252b6
Siva Setti <113090286+sivasetti@users.noreply.github.com>
2026-05-24 23:15:49 +0530

    implemented auth.service for refresh tokens

--- backend/src/modules/auth/auth.service.js
+++ backend/src/modules/auth/auth.service.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcryptjs');
 const authModel = require('./auth.model');
+const pool = require('../../config/db');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
 
 const registerUser= async (data) =>{
     const {name, email, password, role} = data;
@@ -58,9 +61,28 @@ const login = async (data) => {
     }, process.env.JWT_SECRET,{
         expiresIn : process.env.JWT_EXPIRY
     });
+    //Generate refresh token for 7 days
+    const refreshToken = crypto.randomBytes(40).toString('hex');
+
+    //calculate the 7 days deadline date  for MySQL
+
+    const expiresAt = new Date();
+    expiresAt.setDate(expiresAt.getDate() + 7);
+    const mysqlExpiresAt = expiresAt.toISOString().slice(0,19).replace('T', ' ');
+
+
+    // save this into refresh_tokens table
+
+    await pool.query(
+        `INSERT INTO refresh_tokens(user_id, token, expires_at) VALUES (?, ?, ?)`,
+        [user.id, refreshToken, mysqlExpiresAt]
+    );
+
 
     return {
         token,
+        refreshToken,
+        expiresAt,
         user : {
             id : user.id,
             email : user.email,
