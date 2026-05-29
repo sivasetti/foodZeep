a0f2a35599f20e7d3132ef2a9e4e07903daa71ed
Siva Setti <113090286+sivasetti@users.noreply.github.com>
2026-05-29 23:27:52 +0530

    added comments

--- backend/src/modules/auth/auth.routes.js
+++ backend/src/modules/auth/auth.routes.js
@@ -6,11 +6,20 @@ const authMiddleware = require('../../middlewares/auth.middleware.js');
 const validate = require('../../middlewares/validation.middleware.js')
 const {registerSchema, loginSchema}= require('../../validators/auth.validator.js');
 const { valid } = require('joi');
+// ==========================================
+// REGISTRATION & USER MANAGEMENT
+// ==========================================
 
 router.post('/register',validate(registerSchema), authController.register);
 
 router.get('/getAllUsers',authMiddleware.protect, authMiddleware.authorize('Admin'), authController.getUsersAll);
 
+
+
+
+// ==========================================
+// AUTHENTICATION LIFECYCLE
+// ==========================================
 
 router.post('/login',validate(loginSchema), authController.login);
 
